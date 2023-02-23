aa0dca84326af8b3b59dbd7f3355b46eba9db23b
stelmaks <stelmaks101@gmail.com>
2023-02-23 15:47:41 -0800

    Updated

--- history.c
+++ history.c
@@ -97,7 +97,7 @@ build_history_list(info, buf + last, linecount++);
 free(buf);
 info->histcount = linecount;
 while (info->histcount-- >= HIST_MAX)
-delete_node_at_index(&(info->history), 0);
+	delete_node_at_index(&(info->hist), 0);
 renumber_history(info);
 return (info->histcount);
 }
