820523fc6cbe587401c426d745e4275444a349d1
stelmaks <stelmaks101@gmail.com>
2023-02-23 16:59:11 -0800

    Updated

--- history.c
+++ history.c
@@ -4,7 +4,7 @@
  * get_history_file - gets the history file
  * @info: parameter struct
  *
- * Return: allocated string containg history file
+ * Return: allocated string containing history file
  */
 
 char *get_history_file(info_t *info)
@@ -30,6 +30,7 @@ char *get_history_file(info_t *info)
  *
  * Return: 1 on success, else -1
  */
+
 int write_history(info_t *info)
 {
 	ssize_t fd;
@@ -61,58 +62,14 @@ int write_history(info_t *info)
  */
 int read_history(info_t *info)
 {
-<<<<<<< HEAD
-int i, last = 0, linecount = 0;
-ssize_t fd, rdlen, fsize = 0;
-struct stat st;
-char *buf = NULL, *filename = get_history_file(info);
-if (!filename)
-{
-return (0);
-}
-fd = open(filename, O_RDONLY);
-free(filename);
-if (fd == -1)
-{
-return (0);
-}
-if (!fstat(fd, &st))
-fsize = st.st_size;
-if (fsize < 2)
-return (0);
-buf = malloc(sizeof(char) * (fsize + 1));
-if (!buf)
-return (0);
-rdlen = read(fd, buf, fsize);
-buf[fsize] = 0;
-if (rdlen <= 0)
-return (free(buf), 0);
-close(fd);
-for (i = 0; i < fsize; i++)
-if (buf[i] == '\n')
-{
-buf[i] = 0;
-build_history_list(info, buf + last, linecount++);
-last = i + 1;
-}
-if (last != i)
-build_history_list(info, buf + last, linecount++);
-free(buf);
-info->histcount = linecount;
-while (info->histcount-- >= HIST_MAX)
-	delete_node_at_index(&(info->hist), 0);
-renumber_history(info);
-return (info->histcount);
-}
-=======
 	int i, last = 0, linecount = 0;
 	ssize_t fd, rdlen, fsize = 0;
 	struct stat st;
 	char *buf = NULL, *filename = get_history_file(info);
 
 	if (!filename)
+
 		return (0);
->>>>>>> 5b8348e43bd2c3ea141a256f6c07775309f648e1
 
 	fd = open(filename, O_RDONLY);
 	free(filename);
@@ -155,6 +112,7 @@ return (info->histcount);
  *
  * Return: Always 0
  */
+
 int build_history_list(info_t *info, char *buf, int linecount)
 {
 	list_t *node = NULL;
@@ -172,8 +130,10 @@ int build_history_list(info_t *info, char *buf, int linecount)
  * renumber_history - renumbers the history linked list after changes
  * @info: Structure containing potential arguments. Used to maintain
  *
+ *
  * Return: the new histcount
  */
+
 int renumber_history(info_t *info)
 {
 	list_t *node = info->history;
